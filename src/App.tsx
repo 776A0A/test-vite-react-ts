9f96322807aa02f131e36dff322f0d8ebaa7023c
Darenome <har3dy@icloud.com>
2021-05-14 23:05:26 +0800

    feat: 新增home、about、users页面，用以学习react-router

--- src/App.tsx
+++ src/App.tsx
@@ -1,43 +1,43 @@
 import { useState } from 'react'
 import logo from './logo.svg'
 import './App.css'
+import { BrowserRouter as Router, Link, Route, Switch } from 'react-router-dom'
+import { About, Home, Users } from 'views'
 
 function App() {
   const [count, setCount] = useState(0)
 
   return (
     <div className='App'>
-      <header className='App-header'>
-        <img src={logo} className='App-logo' alt='logo' />
-        <p>Hello Vite + React!1244443</p>
-        <p>
-          <button onClick={() => setCount((count) => count + 1)}>
-            count is: {count}
-          </button>
-        </p>
-        <p>
-          Edit <code>App.tsx</code> and save to test HMR updates.
-        </p>
-        <p>
-          <a
-            className='App-link'
-            href='https://reactjs.org'
-            target='_blank'
-            rel='noopener noreferrer'
-          >
-            Learn React
-          </a>
-          {' | '}
-          <a
-            className='App-link'
-            href='https://vitejs.dev/guide/features.html'
-            target='_blank'
-            rel='noopener noreferrer'
-          >
-            Vite Docs
-          </a>
-        </p>
-      </header>
+      <Router>
+        <div>
+          <nav>
+            <ul>
+              <li>
+                <Link to='/'>Home</Link>
+              </li>
+              <li>
+                <Link to='/about'>About</Link>
+              </li>
+              <li>
+                <Link to='/users'>Users</Link>
+              </li>
+            </ul>
+          </nav>
+        </div>
+
+        <Switch>
+          <Route path='/about'>
+            <About />
+          </Route>
+          <Route path='/users'>
+            <Users />
+          </Route>
+          <Route path='/'>
+            <Home />
+          </Route>
+        </Switch>
+      </Router>
     </div>
   )
 }
