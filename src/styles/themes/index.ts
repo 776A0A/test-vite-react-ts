ba8500d43109e2cc99cfd6ba1f1b56b6bfd3d798
Darenome <har3dy@icloud.com>
2021-05-19 22:29:51 +0800

    feat(themes): 添加theme字符串的缓存

--- src/styles/themes/index.ts
+++ src/styles/themes/index.ts
@@ -1,9 +1,6 @@
 import { defaultTheme } from './default'
 import { darkTheme } from './dark'
 
-export * from './default'
-export * from './dark'
-
 type ThemeNames = keyof typeof themes
 
 const themes = {
@@ -20,7 +17,9 @@ export const setTheme = (themeName: ThemeNames, initial = false) => {
 
   const theme = themes[themeName]
 
-  let style = document.getElementById('theme-style')
+  let style = document.getElementById('theme-style') as
+    | (HTMLStyleElement & { themes?: { [key: string]: string } })
+    | null
 
   if (!style) {
     style = document.createElement('style')
@@ -28,8 +27,21 @@ export const setTheme = (themeName: ThemeNames, initial = false) => {
     document.head.appendChild(style)
   }
 
-  style.innerText = `:root{${Object.entries(theme).reduce(
-    (styles, [property, value]) => `${styles}${property}:${value};`,
-    ''
-  )}}`
+  let themeText = ''
+
+  if (style.themes?.[themeName]) {
+    themeText = style.themes[themeName]
+  } else {
+    themeText = `:root{${Object.entries(theme).reduce(
+      (styles, [property, value]) => `${styles}${property}:${value};`,
+      ''
+    )}}`
+
+    // 生成的theme字符串做一个缓存
+    Object.assign(style.themes || (style.themes = {}), {
+      [themeName]: themeText,
+    })
+  }
+
+  style.innerText = themeText
 }
